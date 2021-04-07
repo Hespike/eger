cfe024184bbb99c91397d9f030f0ee180aa7e1ae
Hespi <sardi.andras@gmail.com>
2021-04-07 14:13:36 +0200

    Create Eger.java

---
+++ Eger.java
@@ -0,0 +1,30 @@
+public class Eger {
+    static int kurzormozgatas(Pozicio a, Pozicio b) {
+        int x = b.x - a.x;
+        int y = b.y - a.y;
+
+        if (x == 0 && y == 0) {
+            return 0;
+        }
+
+        if (x == 0) {
+            return y > 0 ? 90 : 270;
+        }
+
+        if (y == 0) {
+            return x > 0 ? 0 : 180;
+        }
+
+        double res = Math.atan(y/(x+0.0)) * 180 / Math.PI;
+
+        if (x > 0 && y > 0) {
+            return (int) Math.round(res);
+        }
+
+        if (x > 0) {
+            return (int) Math.round(res + 360);
+        }
+
+        return (int) Math.round(res + 180);
+    }
+}
